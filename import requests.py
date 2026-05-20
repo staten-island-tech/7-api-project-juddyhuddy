0e445b94295142b4aa69a10f652efa5662147d84
juddyhuddy <justinl732@nycstudents.net>
2026-05-19 22:26:18 -0400

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,4 +1,7 @@
 import requests
-
-def getdisney(disney):
-    response = requests.get(f"https://api.disneyapi.dev/character") 
+url = "https://racinghub.net/api/v1/drivers?page=1&limit=100&order_by=name&sort_by=asc"
+def getf1(f1):
+    response = requests.get(f"https://racinghub.net/api/v1/drivers?page=1&limit=100&order_by=name&sort_by=asc") 
+    data = response.json()
+for driver in data["data"]:
+    print(driver["name"])
