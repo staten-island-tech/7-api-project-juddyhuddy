5ad3bf2a308b2e02871fc0a6440e914cafbea516
juddyhuddy <justinl732@nycstudents.net>
2026-05-21 09:16:55 -0400

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -2,3 +2,15 @@ import requests
 
 def getdisney(disney):
     response = requests.get(f"https://api.disneyapi.dev/character") 
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+      data = response.json()
+    return {
+        "name": data["name"],
+        "height": data["height"],
+        "weight": data["weight"],
+        "types": [t["type"]["name"] for t in data["types"]]
+    }
+pokemon = getPoke("Bulbasaur")
+print(pokemon)
