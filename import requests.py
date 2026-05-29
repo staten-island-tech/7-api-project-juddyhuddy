5f9af0ded080ca87d3f44179e9b3b7910e462dc0
juddyhuddy <justinl732@nycstudents.net>
2026-05-29 08:47:17 -0400

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,16 +1,25 @@
 import requests
 
-def getdisney(disney):
-    response = requests.get(f"https://api.disneyapi.dev/character") 
+
+def getf1(f1):
+    response = requests.get(f"https://racinghub.net/api/v1/drivers?page=1&limit=100&order_by=name&sort_by=asc") 
     if response.status_code != 200:
-        print("Error fetching data!")
+        print('Error fetching data')
         return None
-      data = response.json()
-    return {
-        "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
-    }
-pokemon = getPoke("Bulbasaur")
-print(pokemon)
+    
+    total_championships = 0
+
+    data = response.json()
+    for driver in data["data"]:
+        name = driver["name"]
+        championships = driver.get("world_championship", 0)
+        print(name)
+        print(championships)
+        total_championships += championships
+        drivers.append([name, championships])
+    print(total_championships)
+print("drivers with 0 championships:")
+drivers = []
+for driver in drivers:
+    if driver[1] == 0:
+        print (driver[0])
